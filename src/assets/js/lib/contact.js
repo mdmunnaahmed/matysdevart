9f4365fd60962ea6de406469faa65618d4594445
mu2na <msdmunna77@gmail.com>
2022-08-12 20:59:01 +0600

    contact form running

--- src/assets/js/lib/contact.js
+++ src/assets/js/lib/contact.js
@@ -1,53 +1,51 @@
 (function ($) {
-    "use strict";
-        $(document).on('submit','#contact_form_submit',function(e){
-            e.preventDefault();
-            var name = $('#contact_name').val();
-            var email = $('#contact_email').val();
-            var message = $('#contact_message').val();
-            if (!name) {
-                 $('#contact_name').removeClass('error');
-                 $('#contact_name').addClass('error').attr('placeholder','Please Enter Name');
-             }else{
-                 $('#contact_name').removeClass('error');
-             }
-            if (!email) {
-                 $('#contact_email').removeClass('error');
-                 $('#contact_email').addClass('error').attr('placeholder','Please Enter Email')
-             }else{
-                 $('#contact_email').removeClass('error');
-             }
-            if (!message) {
-                 $('#contact_message').removeClass('error');
-                 $('#contact_message').addClass('error').attr('placeholder','Please Enter Your Message')
-             }else{
-                 $('#contact_message').removeClass('error');
-             }
-            if ( name && email && message ) {
-                 $.ajax({
-                     type: "POST",
-                     url:'../../contact.php',
-                     data:{
-                         'name': name,
-                         'email': email,
-                         'message': message,
-                     },
-                     success:function(data){
-                         $('#contact_form_submit').children('.email-success').remove();
-                         $('#contact_form_submit').prepend('<span class="alert alert-success email-success contact-alert">'+data+'</span>');
-                         $('#contact-name').val('');
-                         $('#contact-email').val('');
-                         $('#contact-message').val('');
-                         $('#contact-map').height('576px');
-                         $('.email-success').fadeOut(3000);
-                     },
-                     error:function(res){
-                     }
-                 });
-             }else{
-                $('#contact_form_submit').children('.email-success').remove();
-                $('#contact_form_submit').prepend('<span class="alert alert-danger email-success contact-alert">Somenthing went wrong</span>');
-                $('.email-success').fadeOut(3000);
-             }
-        });
-}(jQuery));
+	"use strict";
+	$(document).on("submit", "#contact_form", function (e) {
+		e.preventDefault();
+		var name = $("#name").val();
+		var email = $("#email").val();
+		var message = $("#message").val();
+		if (!name) {
+			$("#name").removeClass("error");
+			$("#name").addClass("error").attr("placeholder", "Please Enter Name");
+		} else {
+			$("#name").removeClass("error");
+		}
+		if (!email) {
+			$("#email").removeClass("error");
+			$("#email").addClass("error").attr("placeholder", "Please Enter Email");
+		} else {
+			$("#email").removeClass("error");
+		}
+		if (!message) {
+			$("#message").removeClass("error");
+			$("#message").addClass("error").attr("placeholder", "Please Enter Your Message");
+		} else {
+			$("#message").removeClass("error");
+		}
+		if (name && email && message) {
+			$.ajax({
+				type: "POST",
+				url: "../../contact.php",
+				data: {
+					name: name,
+					email: email,
+					message: message,
+				},
+				success: function (data) {
+					$("#contact_form").children(".email-success").remove();
+					$("#contact_form").prepend('<span class="alert alert-success email-success contact-alert">' + data + "</span>");
+					$("#name").val("");
+					$("#email").val("");
+					$("#message").val("");
+					$(".email-success").fadeOut(3000);
+				},
+				error: function (res) {},
+			});
+		} else {
+			$("#contact_form").children(".email-success").remove();
+			$("#contact_form").prepend('<span class="alert alert-danger email-success contact-alert">Somenthing went wrong</span>');
+			$(".email-success").fadeOut(3000);
+		}
+	});
+})(jQuery);
